57a2b5d7645b683c7be9b5531b988160c473132e
yuwen <yuwen.azulejos@gmail.com>
2024-04-16 16:15:33 +0800

    feat: handle async planets stream before serving

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,54 @@
-const planets = ['planet1', 'planet2', 'planet3'];
+// const planets = ['planet1', 'planet2', 'planet3'];
+// module.exports = planets;
 
-module.exports = planets;
+const fs = require('fs');
+const path = require('path');
+const {parse} = require('csv-parse');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+/**
+ * 因為streams是async的，當node不會等他做完，才module export。
+ * 我們希望server先處理完planets，再開始serve資料。
+ * 
+ * 需要等待的code -> 用async/await -> 但是對象必須是promise -> return一個promise
+ */
+
+function loadPlanetsData() {
+  console.log('Loading planet data...');
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+      .pipe(parse({
+        comment: '#',
+        columns: true,
+      }))
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on('error', (err) => {
+        console.log(err);
+      })
+      .on('end', () => {
+        // console.log(habitablePlanets.map((planet) => {
+        //   return planet['kepler_name'];
+        // }));
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve(); 
+        // 通常resolve()裡面會接回傳的值，
+        // 但我們會另外export出habitablePlanets，所以這裡不用回傳
+    });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 // const express = require('express'); // 拆分code到app.js
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.model');
 
 // const app = express();
 
@@ -13,11 +14,16 @@ const server = http.createServer(app);
 // console出來的port會是字串5000
 const PORT = process.env.PORT || 8001;
 
+// 為了解決planets串流還沒完成，就收到request的問題
+// 把planets先處理好(會是一個promise)，再開始serve
+const startServer = async () => {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
 
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
 
 // ==== 1. 原本的作法，使用express ====
 // const express = require('express');
