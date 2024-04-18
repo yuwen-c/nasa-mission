cb61b03903beabce38db8f0e36354fb6971044b7
yuwen <yuwen.azulejos@gmail.com>
2024-04-18 12:27:15 +0800

    feat: morgan log middleware; launches model

---
+++ README.md
@@ -0,0 +1,10 @@
+# 起專案
+
+## 現在在root就可以做到，起一個server就好
+
+- 開發階段：npm run server
+
+## 舊的啟動作法，分兩個server
+
+- 進到client(front end): npm start (running on localhost:3000)
+- 進到server(back end): npm run watch (running on localhost:8000)
--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
