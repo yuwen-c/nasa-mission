be1225c735d95863f4c5b42002bae7fe047e3a8c
yuwen <yuwen.azulejos@gmail.com>
2024-04-18 12:09:17 +0800

    feat: automation of script in root package.json

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -6,7 +7,13 @@ const app = express();
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
+// 好像沒放也可以？
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 app.use(planetsRouter);
 
 module.exports = app;
